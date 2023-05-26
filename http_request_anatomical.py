b476deccb9fe7986279d3e8d1be82cc942664745
Abdullah Sbeata <114855752+AbdullahSbeata@users.noreply.github.com>
2023-05-27 01:07:48 +0300

    Update http_request_anatomical.py
    Just Simplify the code.

--- http_request_anatomical.py
+++ http_request_anatomical.py
@@ -1,25 +1,16 @@
 class HttpAnatomical():
     def __init__(self, request) -> None:
-        self.__headers = {}
         self.__query_param = None
-        self.__body = {}
-        self.__dissect_request(request)
-    
+        self.__dissect_request(request)  
+
     def __dissect_request(self, request):
         headers, *body = request.split("\r\n\r\n")
         headers = headers.split("\r\n")
         self.__fetch_query_param(headers[0])
 
-    
     def get_query_param(self):
-        return self.__query_param
+        return self.__query_param   
     
     def __fetch_query_param(self, http_head):
         parts = http_head.split(' ')
         self.__query_param = parts[1][1:]
-    
-    def get_headers(self):
-        return self.__headers
-    
-    def get_body(self):
-        return self.__body
